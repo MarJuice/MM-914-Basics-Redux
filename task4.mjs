687b4d512e1ff5fe40d6057d4b624258baffa6c9
MarJuice <mariushanss1@gmail.com>
2025-02-12 10:13:37 +0100

    Create task files

---
+++ task4.mjs
@@ -0,0 +1,17 @@
+function move() {
+    // Moves the car 1 cell in the direction it is heading. 
+}
+
+function turn() {
+    // Turns the car 90 deg clockwise.
+}
+
+function peek() {
+    // Returns true if the next cell is open, otherwise false.
+    return true; // Just a placeholder value. 
+}
+
+function atGoal() {
+    // Returns true if the current cell is the goal cell.
+    return true; // just a placholder
+}

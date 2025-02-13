bd32dc3c5652409471218897d19b57b6354eaf3c
Marius <79099572+MarJuice@users.noreply.github.com>
2025-02-13 10:42:57 +0100

    Create file for game constants

---
+++ gameConstants.mjs
@@ -0,0 +1,19 @@
+// Variables
+let playerPos = {
+    row: null,
+    col: null
+}
+
+// Game constants
+const CAR = "🚗";
+const GOAL = "🏁";
+const WALL = "⬜";
+const EMPTY = "⬛";
+
+export {
+    playerPos,
+    CAR,
+    GOAL,
+    WALL,
+    EMPTY
+};

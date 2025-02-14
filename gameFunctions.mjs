58f305ee7932c522880a59602ec17ac74b3919cc
Marius <79099572+MarJuice@users.noreply.github.com>
2025-02-14 10:54:06 +0100

    Move atGoal function back to task file, store goal position
    The atGoal function caused issues because the interval is in the task file, so I moved it back there. The goalPos array is to fix a bug where the car would just drive over the goal and replace it with EMPTY, because of current position and next position.

--- gameFunctions.mjs
+++ gameFunctions.mjs
@@ -3,16 +3,9 @@ import { CAR, GOAL, playerPos } from "./gameConstants.mjs";
 
 let map = []; // Empty array to store rows and columns
 let rawMap = maps.map1.split("\n"); // Separate each row of the map into an array
+let goalPos = []; // Empty array to store the goal position
 for (let i = 0; i < rawMap.length; i++) { // Iterate over each row
     map.push([...rawMap[i]]); // Push the rows into the map array
-}
-
-function atGoal() {
-    // Returns true if the current cell is the goal cell.
-    if (map[playerPos.row][playerPos.col] == GOAL) {
-        return true;
-    }
-    return false;
 }
 
 function update() { // Checks every row and column
@@ -21,6 +14,8 @@ function update() { // Checks every row and column
             if (map[row][col] == CAR) { // Assigns the player position based on where the car is
                 playerPos.row = row;
                 playerPos.col = col;
+            } else if (map[row][col] == GOAL) { // Assigns the goal position based on where the goal is
+                goalPos.push(row, col);
             }
         }
     }
@@ -28,9 +23,6 @@ function update() { // Checks every row and column
 
 function drawMap() { // Draws the updated array
     console.log(map.map(row => row.join("")).join("\n")); // Draws a formatted array by joining rows and columns into a single string
-    if (atGoal()) { // If the atGoal function returns true, it ends the loop
-        clearInterval(interval); // Clears the interval in the task
-    }
 }
 
-export { map, update, drawMap };
+export { goalPos, map, update, drawMap };
